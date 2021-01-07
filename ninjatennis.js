d55c142224467dc19f244f6c5562c0c47b906f72
peterle2011 <35751000+peterle2011@users.noreply.github.com>
2021-01-07 10:01:09 -0800

    j

--- ninjatennis.js
+++ ninjatennis.js
@@ -5,7 +5,7 @@ function getMatchHTML(match) {
   <div class="col-sm-12 col-md-6 col-lg-4">
     <div class="container">
       <div class="row scorebox">
-        <div class="scorebox_pic border border-success bg-info text-white">p</div>
+        <div class="scorebox_pic border border-success bg-info text-white">P</div>
         <div class="scorebox_name border border-success bg-info text-white">${match.player_1}</div>
         <div class="scorebox_set border border-success bg-info text-white">${match.set_1[0]}</div>
         <div class="scorebox_set border border-success bg-info text-white">${match.set_2[0]}</div>
@@ -14,7 +14,7 @@ function getMatchHTML(match) {
         <div class="scorebox_set border border-success bg-info text-white">${match.set_5[0]}</div>
       </div>
       <div class="row scorebox">
-          <div class="scorebox_pic border border-danger">p</div>
+          <div class="scorebox_pic border border-danger">P</div>
           <div class="scorebox_name border border-danger">${match.player_2}</div>
           <div class="scorebox_set border border-danger">${match.set_1[1]}</div>
           <div class="scorebox_set border border-danger">${match.set_2[1]}</div>
